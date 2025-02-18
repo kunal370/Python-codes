b91e604ef34782cfcf3f46a87cf59dd643b65d4e
KUNAL MESHRAM ⚡⚡ <85063979+kunal370@users.noreply.github.com>
2025-02-18 16:38:56 +0530

    Update 3. multiple value assign_data type_type casting.py

--- 3. multiple value assign_data type_type casting.py
+++ 3. multiple value assign_data type_type casting.py
@@ -46,3 +46,16 @@ print('*****************************************************')
 
 price=59
 print(f'price of product is {price:.4f}')#.4f--> refers to how many decimals we want 
+print('*****************************************************')
+
+print('operations for Complex data type')
+num1=3+4j
+num2=5+7j
+num3=num1+num2  #5+3=8, 4+7=11,...==>8+11j
+num4=num1*num2# 3*5=15, 4*7=28, 15-28=-13, 3*7=21, 4*5=20 , 21+20=41.....==>-13+41j
+num5=num1/num2
+print('addition is',num3)
+print('multiplicatiob is',num4)
+print('division is',num5)
+print('*****************************************************')
+
